24f0b84b72898bcb9620e1fa08de7c1e18745632
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-09-21 00:52:44 +0500

    Initial commit

---
+++ Assets/Scripts/ServerTime.cs
@@ -0,0 +1,61 @@
+using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerTime
+{
+    private string url;
+    private int timezoneOffset;
+    private DateTime time;
+
+    public ServerTime(string url, int timezoneOffset = 0)
+    {
+        this.url = url;
+        this.timezoneOffset = timezoneOffset;
+    }
+
+    public async UniTask<DateTime> GetCurrentTime()
+    {
+        await CheckGlobalTimeAsync();
+        return time;
+    }
+    
+    private async UniTask CheckGlobalTimeAsync()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            await request.SendWebRequest();
+
+            // Проверяем на ошибки запроса
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error fetching time from server: " + request.error);
+
+                // Используем локальное системное время
+                time = DateTime.Now.AddHours(timezoneOffset);
+                Debug.Log("Using local system time: " + time.ToString("HH:mm:ss"));
+                return;
+            }
+
+            // Извлекаем заголовок "Date" из ответа
+            if (request.GetResponseHeaders().TryGetValue("Date", out string timeStr))
+            {
+                if (DateTime.TryParse(timeStr, out DateTime globDateTime))
+                {
+                    // Время в UTC
+                    time = globDateTime.ToUniversalTime().AddHours(timezoneOffset);
+                    Debug.Log(time.ToString("HH:mm:ss"));
+                }
+                else
+                {
+                    Debug.LogError("Failed to parse the date from the response.");
+                }
+            }
+            else
+            {
+                Debug.LogError("Date header not found in the response.");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private string url;
+    [SerializeField] [Range(-11, 11)] private int timezoneOffset;
+    
+    private ServerTime _serverTime;
+    
+    private void Awake()
+    {
+        _serverTime = new ServerTime(url, timezoneOffset);
+        GetCurrentTime();
+    }
+
+    public UniTask<DateTime> GetCurrentTime() => _serverTime.GetCurrentTime();
+}
