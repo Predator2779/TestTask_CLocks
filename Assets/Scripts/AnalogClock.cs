a6ce8c2dab047fd58cc71aabb5aecfae9744f846
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-09-21 03:27:12 +0500

    added analog clock

---
+++ Assets/Scripts/AnalogClock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AnalogClocks : MonoBehaviour
+{
+    [SerializeField] private TimeKeeper timeKeeper;
+    [SerializeField] private Transform hourHand, minuteHand, secondHand;
+
+    private const float hoursDegree = 360f / 12f, minutesDegree = 360f / 60f, secondsDegree = 360f / 60f;
+
+    private void FixedUpdate()
+    {
+        var time = timeKeeper.CurrentTime;
+        RotateHand(hourHand, time.Hour * -hoursDegree);
+        RotateHand(minuteHand, time.Minute * -minutesDegree);
+        RotateHand(secondHand, time.Second * -secondsDegree);
+    }
+
+    private void RotateHand(Transform hand, float angle) =>
+        hand.localRotation = Quaternion.Euler(0f, 0f, angle);
+}
--- Assets/Scripts/ServerTime.cs
+++ Assets/Scripts/ServerTime.cs
@@ -27,26 +27,20 @@ public class ServerTime
         using (UnityWebRequest request = UnityWebRequest.Get(url))
         {
             await request.SendWebRequest();
-
-            // Проверяем на ошибки запроса
+            
             if (request.result != UnityWebRequest.Result.Success)
             {
                 Debug.LogError("Error fetching time from server: " + request.error);
-
-                // Используем локальное системное время
+                
                 time = DateTime.Now.AddHours(timezoneOffset);
-                Debug.Log("Using local system time: " + time.ToString("HH:mm:ss"));
                 return;
             }
-
-            // Извлекаем заголовок "Date" из ответа
+            
             if (request.GetResponseHeaders().TryGetValue("Date", out string timeStr))
             {
                 if (DateTime.TryParse(timeStr, out DateTime globDateTime))
                 {
-                    // Время в UTC
                     time = globDateTime.ToUniversalTime().AddHours(timezoneOffset);
-                    Debug.Log(time.ToString("HH:mm:ss"));
                 }
                 else
                 {
